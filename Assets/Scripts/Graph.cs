a700d34c9047c55bcc418e77a97ef84604a72ef7
klokMiraj <pudasainimetal96@gmail.com>
2019-12-19 23:15:52 +0545

    added heuristic and path finders

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Graph
+{
+    // A list of graph connections.
+    private List<SheetMover> WaypointConnections = new List<SheetMover>();
+    public Graph()
+    {
+    }
+    // Add connection.
+    public void AddConnection(SheetMover aConnection)
+    {
+        WaypointConnections.Add(aConnection);
+    }
+    // Get the connections from a node to the nodes it is connected to.
+    public List<SheetMover> GetConnections(GameObject FromNode)
+    {
+        List<SheetMover> TmpConnections = new List<SheetMover>();
+        foreach (SheetMover aConnection in WaypointConnections)
+        {
+            if (aConnection.GetFromNode().Equals(FromNode))
+            {
+                TmpConnections.Add(aConnection);
+            }
+        }
+        return TmpConnections;
+    }
+}
